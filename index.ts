718ea542ae175a6e646b4b357570333c8752ee8e
henriquegomes6 <henriquegomes6@hotmail.com>
2023-10-23 17:24:27 -0300

    add discord

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './helpers/functions';
+export * from './helpers/settings';
 export * from './page/AbstractPage';
 export * from './page/types';
-
+export * from './reporters/DiscordReporter';
